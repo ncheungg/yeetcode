2ce46ac9bf032399669219491b9b866fdfb9531a
Jessica Lui <44094521+jesskeepswimming@users.noreply.github.com>
2022-12-19 18:10:50 -0500

    functions work

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -12,6 +12,7 @@ const config = (env, argv) =>
       popup: PATHS.src + '/popup.ts',
       contentScript: PATHS.src + '/contentScript.ts',
       background: PATHS.src + '/background.ts',
+      sidebar: PATHS.src + '/sidebar.ts',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
   });
