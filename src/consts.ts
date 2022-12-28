6b4f69c2d03bc0efc9e2421d14f1361c5b8778d8
Nathan Cheung <nathancheung22@gmail.com>
2022-12-28 15:42:48 -0500

    added new websocket host using northflank

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 // export const HOST = 'localhost';
 export const HOST = 'yeetco.de';
-export const WS_HOST = 'yeetcode-backend.onrender.com';
+// export const WS_HOST = 'localhost:1234';
+// export const WS_HOST = 'yeetcode-backend.onrender.com';
+export const WS_HOST = 'wsp--yeetcode--5pdr84rrcbjx.code.run';
